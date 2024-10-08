d5a335c60f51f99f0832934a1734ab392c5d4336
Bunu <bunubhattarai39@gmail.com>
2024-10-08 19:42:52 -0400

    updated

---
+++ easy/minimum-number-of-swaps-to-make-the-string-balanced/MinimumNumberOfSwap.java
@@ -0,0 +1,50 @@
+public class MinimumNumberOfSwap {
+    public static void main(String[] args) {
+        MinimumNumberOfSwap solution = new MinimumNumberOfSwap();
+
+        System.out.println(solution.minSwaps("]][][["));
+    }
+
+    public int minSwaps(String s) {
+
+        int balanceCounter = 0;
+        int swapNeeded = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '[') {
+                balanceCounter++;
+            } else {
+                balanceCounter--;
+            }
+            if (balanceCounter < 0) {
+                swapNeeded++;
+                balanceCounter += 2;
+            }
+        }
+        return swapNeeded;
+    }
+}
+
+/*
+ * class Solution {
+ * public int minSwaps(String s) {
+ * 
+ * // keep track of unmatched opening brackets
+ * Stack<Integer> stack=new Stack<>();
+ * 
+ * for(char c: s.toCharArray()){
+ * if(c=='['){
+ * stack.push(1);
+ * }else{
+ * if(!stack.isEmpty()){
+ * stack.pop();
+ * }else{
+ * stack.push(-1);
+ * }
+ * }
+ * }
+ * int unmatched=stack.size();
+ * return unmatched/2;
+ * }
+ * }
+ */
--- easy/minimum-number-of-swaps-to-make-the-string-balanced/Solutiton.java
+++
@@ -1,5 +0,0 @@
-package easy.minimum-number-of-swaps-to-make-the-string-balanced;
-
-public class Solutiton {
-
-}
