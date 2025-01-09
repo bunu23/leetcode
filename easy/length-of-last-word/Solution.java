6c678691963de6e6eec38c3c92c31122c5ba8dc8
Bunu <bunubhattarai39@gmail.com>
2025-01-08 21:32:34 -0500

    length of last word

---
+++ easy/length-of-last-word/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        int lastSpaceIndex = s.lastIndexOf(' ');
+        return s.length() - lastSpaceIndex - 1;
+
+    }
+}
